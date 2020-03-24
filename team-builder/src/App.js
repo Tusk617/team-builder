beb7a00aeea674e02d2801773eff30af1433ece6
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-03-24 19:16:27 -0400

    Completed MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -4,21 +4,27 @@ import { v4 as uuidv4 } from 'uuid';
 import Form from './Components/Forms'
 
 var initialTeam = [
-  {id: uuidv4(), fullName: 'Kory Hacker', email: 'kory.hacker@gmail.com', role: 'Da brains'}
+  {id: uuidv4(), fullName: 'Kory Hacker', email: 'kory.hacker@gmail.com', role: 'Frontend'}
 
 ]
 
 function App() {
-
+  const [members, setMembers] = useState(initialTeam);
+console.log(members);
+  
   
   return (
     <div className="App">
 
-      <Form />
+      <Form 
+        setMembers={setMembers}
+        members={members}
+      
+      />
 
       <h1>Team List</h1>
-      {initialTeam.map(member => 
-        <div key={member.id}> {member.fullName}, {member.email}, {member.role} </div>
+      {members.map(mem => 
+        <div key={mem.id}> {mem.fullName}, {mem.email}, {mem.role} </div>
         )}
     </div>
   );
--- team-builder/src/Components/Forms.js
+++ team-builder/src/Components/Forms.js
@@ -1,13 +1,48 @@
 import React, {useState} from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 
 
 function Form(props) {
+
+    console.log(props)
+    const [formData, setFormData] = useState({
+        role: '',
+        fullName: '',
+        email: ''
+    })
+
+    console.log(formData)
+
+    const onInputChange = event => {
+
+        const inputThatChanged = event.target.name
+        const newValueForInput = event.target.value
+        setFormData({
+            ...formData,
+            [inputThatChanged]: newValueForInput,
+        })
+    }
+    
+    
+    const onFormSubmit = event => {
+        event.preventDefault()
+
+        const newMember = {
+            id: uuidv4(),
+            role: formData.role,
+            fullName: formData.fullName,
+            email: formData.email,
+        }
+        
+        props.setMembers([...props.members, newMember])
+    }
+
     return (
-    <form>
+    <form onSubmit={onFormSubmit}>
 
         <label> Role: 
-        <select>
+        <select onChange={onInputChange} name="role" value={props.role} defaultValue="">
+            <option></option>
             <option>Backend Engineer</option>
             <option>Frontend Engineer</option>
             <option>UX Designer</option>
@@ -18,12 +53,16 @@ function Form(props) {
 
         <label> Full Name:
             <input 
+                onChange={onInputChange}
+                value={props.fullName}
                 name='fullName'
             />
         </label><br />
 
         <label> E-mail:
             <input 
+                onChange={onInputChange}
+                value={props.email}
                 name='email'
             />
         </label><br />
